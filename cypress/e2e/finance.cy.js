395fa533e7e555b6a8f62edeba5e74f048b47382
Gabriel-P-Freitas <f.pessoa@escolar.ifrn.edu.br>
2025-04-16 09:01:55 -0300

    feat: beforeEach + delete transaction

--- cypress/e2e/finance.cy.js
+++ cypress/e2e/finance.cy.js
@@ -1,20 +1,30 @@
+// const { create } = require("cypress/types/lodash");
+
 describe("Transações", () => {
+  beforeEach(() => {
+    cy.visit("https://devfinance-agilizei.netlify.app/");
+  });
   it("Cadastrar uma entrada", () => {
-    cy.visit("https://devfinance-agilizei.netlify.app/#");
-    cy.get(".button").contains("+ Nova Transação").click();
-    cy.get("#description").type("Freelancer");
-    cy.get("#amount").type(500.0);
-    cy.get("#date").type("2025-03-23");
-    cy.get(".actions > button:nth-child(2), Salvar").click();
-    cy.get("tbody  tr  td.description").should("have.text", "Freelancer");
+    createTransaction("Freelancer", 500, "2025-04-10");
   });
 
   it("Cadastrar uma saída", () => {
-    cy.visit("https://devfinance-agilizei.netlify.app/#");
-    cy.get(".button").contains("+ Nova Transação").click();
-    cy.get("#description").type("Cinema");
-    cy.get("#amount").type(-50);
-    cy.get("#date").type("2025-03-25");
-    cy.get(".actions > button:nth-child(2), Salvar").click();
+    createTransaction("Bolo", -50, "2025-03-23");
+  });
+
+  it.only("Excluir transação", () => {
+    createTransaction("Freelancer", 200, "2025-04-16");
+    cy.get("tr > .description").parent().find("img").click();
+    // cy.get("tr>.description").siblings().children("img").click();
+    // cy.get("tr>td:nth-child(4)>img").click();
   });
 });
+
+function createTransaction(description, value, date) {
+  cy.visit("https://devfinance-agilizei.netlify.app/#");
+  cy.get(".button").contains("+ Nova Transação").click();
+  cy.get("#description").type(description);
+  cy.get("#amount").type(value);
+  cy.get("#date").type(date);
+  cy.get(".actions > button:nth-child(2), Salvar").click();
+}
